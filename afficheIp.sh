6c8d885b4de68eb58ff0d882b798a63b62b1b093
IsakBedard <isakbedard@gmail.com>
2019-12-16 20:40:32 -0500

    partie d'utilisateur faite

--- afficheIp.sh
+++ afficheIp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 touch temp
 ip addr | grep global > temp
-grep -o '[^ ]\+' temp | grep / - > /etc/issue 
+grep -o '[^ ]\+' temp | grep / - > /etc/issue
+
+useradd -g sudo -d /home/Isak -p $(echo "tge243" | openssl passwd -1 -stdin)
+echo 'Isak ALL=(ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
 

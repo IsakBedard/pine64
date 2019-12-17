1104d83554706829278637ea9f8e15152c9e17f5
IsakBedard <isakbedard@gmail.com>
2019-12-16 20:58:07 -0500

    petite correction

--- scriptFinal.sh
+++ scriptFinal.sh
@@ -3,7 +3,7 @@ touch temp
 ip addr | grep global > temp
 grep -o '[^ ]\+' temp | grep / - > /etc/issue
 
-useradd -g sudo -d /home/Isak -p $(echo "tge243" | openssl passwd -1 -stdin)
+useradd -g sudo -d /home/Isak -p $(echo "tge243" | openssl passwd -1 -stdin) -m Isak
 echo 'Isak ALL=(ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
 ssh-keygen
 
